39baa40f4881ed65695b2ca762ca815d582cff35
Tanya <pilipenkotanya75@gmail.com>
2023-06-05 20:34:38 +0300

    Update feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,6 @@
 import throttle from 'lodash.throttle';
 
 const formFeedbackEl = document.querySelector(".feedback-form");
-const inputFeedbackEmailEl = document.querySelector("input");
-const messageFeedbackEl = document.querySelector("textarea");
 
 
 const { email, message } = formFeedbackEl.elements;
@@ -10,38 +8,46 @@ const { email, message } = formFeedbackEl.elements;
 // Зберігаю дані, що були введені в поля форми  у локальне сховище, ключем для сховища буде рядок "feedback-form-state"
 const LOCALSTORAGE_KEY = "feedback-form-state";
 
-// до форми додаємо слухача події на  форму
-formFeedbackEl.addEventListener("submit",  throttle(handleSubmit, 500));
+// до форми додаємо слухача події на  SUBMIT і на  INPUT
+formFeedbackEl.addEventListener("input", throttle(onInputValues, 500));
+
+formFeedbackEl.addEventListener("submit", handleSubmit);
 
 // В змінну отримуємо дані з локального сховища і перетворюємо в валідне значення для JS
 
-const valuesFormSaved = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+let valuesFormSaved = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
 
 reloadPage();
+
+
+// Функція-обробник полів  INPUT та TEXTAREA
+function onInputValues(event) {
+
+event.preventDefault();
+  
+  valuesFormSaved = {
+       email: email.value,
+       message: message.value,
+
+  };
+// записуємо в локальне сховище введені в форму значення
+  
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(valuesFormSaved));
+}
 
 // Функція-обробник кнопки SUBMIT
 
 function handleSubmit(event) {
-    
+
   event.preventDefault();
- 
-    if (inputFeedbackEmailEl.value === "" || messageFeedbackEl.value === "") {
+
+      
+    if (email.value === "" || message.value === "") {
       
     return console.log("Please fill in all the fields!");
     }
-
-     const arrValues = {
-       email: inputFeedbackEmailEl.value,
-       message: messageFeedbackEl.value,
-
-     };
-    
-     // записуємо в локальне сховище введені в форму значення
-  
-    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(arrValues));
-
-    console.log(`Email: ${inputFeedbackEmailEl.value}, Message: ${messageFeedbackEl.value}`);
-     
+        console.log({ email: email.value, message: message.value });     
+         
     // очищуємо дані форми після відправки
     formFeedbackEl.reset();
 
